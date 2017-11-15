cd3297fa053232d550ee0b47a597d6cd47161e56
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2017-11-16 00:59:10 +0330

    add sign in

--- config/passport.js
+++ config/passport.js
@@ -42,3 +42,27 @@ passport.use('local.signup', new LocalStrategy({
         });
     });
 }));
+
+passport.use('local.signin', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+}, function (req, email, password, done) {
+    req.checkBody('email', 'Invali Email').notEmpty().isEmail();
+    req.checkBody('password', 'Invali password').notEmpty().isLength({ min: 4});
+    var errors = req.validationErrors();
+    if(errors) {
+        var messages = [];
+        errors.forEach(function (error) {
+            messages.push(error.msg);
+        });
+        return done(null, false, req.flash('error', messages));
+    }
+    User.findOne({ email: email }, function (err, user) {
+        if(err)
+            return done(err);
+        if(!user || !user.validPassword(password))
+            return done(null, false, { message: 'Invalid Email or Password' });
+        return done(null, user);
+    });
+}));
